ca55b4d393a9b36d5a5afc8f0ef0cd3bd3c063f4
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2023-11-19 19:13:45 +0800

    started InputText Widget

---
+++ paper_ui/lib/components/forms/input.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InputText extends StatefulWidget {
+  final bool? obscureText;
+  final bool? readOnly;
+  final bool? enabled;
+  final String? Function(String?)? validator;
+  final ValueChanged<String> onChange;
+  final InputDecoration? decoration;
+  final String initialValue;
+
+  const InputText.unstyled({
+    super.key,
+    this.obscureText = false,
+    this.readOnly = true,
+    this.enabled = true,
+    this.validator,
+    required this.onChange,
+    this.decoration,
+    required this.initialValue,
+  });
+
+  InputText({
+    super.key,
+    required String hint,
+    required String label,
+    Color? color = Colors.black,
+    Color? errorColor = Colors.redAccent,
+    this.readOnly = false,
+    this.enabled,
+    this.validator,
+    required this.onChange,
+    this.obscureText = false,
+    required this.initialValue,
+  }) : decoration = InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(
+              width: 2.0, // Specify the border width
+            ),
+          ),
+          floatingLabelStyle: GoogleFonts.rubik(color: color, fontSize: 18),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(width: 2.0, color: color!),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(
+                width: 2.0, // Specify the border width
+                color: errorColor!),
+          ),
+          label: Text(label),
+        );
+
+  @override
+  State<InputText> createState() => _InputTextState();
+}
+
+class _InputTextState extends State<InputText> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Set the initial value when the widget is initialized
+    _textEditingController.text = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _textEditingController,
+      obscureText: widget.obscureText!,
+      enabled: widget.enabled,
+      onTapOutside: (PointerDownEvent event) {
+        FocusScope.of(context).unfocus();
+      },
+      cursorColor: Colors.black,
+      decoration: widget.decoration,
+    );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+}
