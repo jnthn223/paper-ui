28bf5eead938588e6b5f87f0f4b2802476c13318
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2023-12-27 09:29:36 +0800

    Alert layout

--- paper_ui/lib/components/feedback/alert.dart
+++ paper_ui/lib/components/feedback/alert.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:paper_ui/components/forms/button.dart';
+import 'package:paper_ui/constants/sizes.dart';
 
 class Alert extends StatelessWidget {
   const Alert({super.key});
@@ -7,8 +9,48 @@ class Alert extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      // use flutter's dialog
-      child: Text("Hello"),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+          side: BorderSide(color: Colors.black, width: 2)),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Are you sure?",
+              style: GoogleFonts.rubik(
+                  fontSize: getFontSize(Sizes.x2l),
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text("This cannot be undone. Do you want to proceed?"),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button(
+                  onPressed: () {},
+                  text: "Nope",
+                  size: Sizes.md,
+                ),
+                Button(
+                  onPressed: () {},
+                  text: "Yes",
+                  bgColor: Colors.black,
+                  color: Colors.white,
+                  size: Sizes.md,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
