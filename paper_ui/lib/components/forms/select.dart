7ce0c98822266b54901f656476eb36b9485e7646
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2024-01-13 20:50:03 +0800

    update select widget

--- paper_ui/lib/components/forms/select.dart
+++ paper_ui/lib/components/forms/select.dart
@@ -6,12 +6,18 @@ import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 @UseCase(name: "Select", type: Select)
 Widget defaultSelect(BuildContext context) {
-  return Select(
-    options: ["Grapes", "Apple", "Orange"],
-    selectedValue: "Grapes",
-    onChanged: (String? value) {},
-    iconBackgroundColor: context.knobs.colorOrNull(
-        label: "Icon Background Color", initialValue: Colors.greenAccent),
+  return LayoutBuilder(
+    builder: (_, constraints) => Select(
+      width: constraints.maxWidth,
+      options: ["Grapes", "Apple", "Orange"],
+      selectedValue: "Grapes",
+      onChanged: (String? value) {},
+      size: Sizes.md,
+      hint: context.knobs
+          .stringOrNull(label: "Hint", initialValue: "Select Fruit"),
+      iconBackgroundColor: context.knobs.colorOrNull(
+          label: "Icon Background Color", initialValue: Colors.greenAccent),
+    ),
   );
 }
 
@@ -22,6 +28,7 @@ class Select extends StatelessWidget {
   final String? hint;
   final Sizes? size;
   final Color? iconBackgroundColor;
+  final double? width;
 
   const Select({
     super.key,
@@ -31,27 +38,31 @@ class Select extends StatelessWidget {
     this.hint,
     this.size = Sizes.sm,
     this.iconBackgroundColor = Colors.greenAccent,
+    this.width,
   });
 
   @override
   Widget build(BuildContext context) {
+    print(width);
     return DropdownMenu(
-      menuStyle: MenuStyle(),
-      enableFilter: true,
-      requestFocusOnTap: true,
+      menuStyle: MenuStyle(visualDensity: VisualDensity.compact),
+      width: width,
       inputDecorationTheme: const InputDecorationTheme(
-        // filled: true,
-        contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+        border: OutlineInputBorder(),
       ),
-      textStyle: GoogleFonts.rubik(fontSize: getFontSize(size)),
+      textStyle: GoogleFonts.rubik(),
+      enableSearch: false,
+      enableFilter: false,
+      requestFocusOnTap: false,
+      hintText: hint!,
       dropdownMenuEntries: options.map(
         (String value) {
           return DropdownMenuEntry<String>(
             labelWidget: Text(
               value,
               style: GoogleFonts.rubik(
-                fontSize: getFontSize(size),
-              ),
+                  // fontSize: getFontSize(size),
+                  ),
             ),
             value: value,
             label: value,
@@ -59,47 +70,6 @@ class Select extends StatelessWidget {
         },
       ).toList(),
     );
-    // return DropdownButtonFormField(
-    //   items: options.map<DropdownMenuItem<String>>((String value) {
-    //     return DropdownMenuItem<String>(
-    //       value: value,
-    //       child: selectText(value),
-    //     );
-    //   }).toList(),
-    //   isExpanded: true,
-    //   isDense: false, // Set this to false to make the dropdown width extended
-    //   itemHeight: null,
-    //   decoration: InputDecoration(
-    //     hintText: hint,
-    //     filled: true,
-    //     fillColor: Colors.red,
-    //   ),
-    //   alignment: Alignment.center,
-    //   dropdownColor: Colors.blue,
-    //   onChanged: onChanged,
-    // );
-    // return DropdownButtonFormField<String>(
-    //   value: selectedValue,
-    //   onChanged: onChanged,
-    //   hint: hint != null ? selectText(hint!) : null,
-    // items: options.map<DropdownMenuItem<String>>((String value) {
-    //   return DropdownMenuItem<String>(
-    //     value: value,
-    //     child: selectText(value),
-    //   );
-    // }).toList(),
-    //   elevation: 0,
-    //   focusColor: Colors.red,
-    //   decoration: InputDecoration(
-    //     focusedBorder: selectOutlineBorder(),
-    //     enabledBorder: selectOutlineBorder(),
-    //     suffixIcon: selectSuffixIcon(),
-    //     contentPadding: const EdgeInsets.symmetric(
-    //         vertical: 5, horizontal: 5), // Adjust padding as needed
-    //   ),
-    //   icon: const SizedBox.shrink(),
-    //   // isExpanded: true,
-    // );
   }
 
   Text selectText(String value) {
@@ -108,44 +78,7 @@ class Select extends StatelessWidget {
       style: GoogleFonts.rubik(
         color: Colors.black,
         fontSize: getFontSize(size),
-        height: 1.0,
       ),
-    );
-  }
-
-  Container selectSuffixIcon() {
-    return Container(
-      width: 25,
-      height: 60,
-      margin: EdgeInsets.zero,
-      decoration: BoxDecoration(
-        color: iconBackgroundColor,
-        border: Border.all(color: Colors.black, width: 2),
-        borderRadius: const BorderRadius.only(
-          topRight: Radius.circular(8),
-          bottomRight: Radius.circular(8),
-          topLeft: Radius.circular(0),
-          bottomLeft: Radius.circular(0),
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0), // Add padding around the icon
-        child: Icon(
-          Icons.arrow_drop_down,
-          color: Colors.black,
-          size: getFontSize(size) + 5,
-        ),
-      ),
-    );
-  }
-
-  OutlineInputBorder selectOutlineBorder() {
-    return const OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.black,
-        width: 2.0,
-      ),
-      borderRadius: BorderRadius.all(Radius.circular(8)),
     );
   }
 }
